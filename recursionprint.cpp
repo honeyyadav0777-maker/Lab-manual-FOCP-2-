c31496310b2b0d806a0b735456a8d9a962400c29
HONEY YADAV <honeyyadav0777@gmail.com>
2026-03-12 17:40:06 +0530

    Add files via upload

---
+++ recursionprint.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using  namespace std;
+void print(int n){
+   if(n==0) return; 
+    print(n-1);
+   cout<<n<<endl;  
+}
+int main(){
+int x;
+cout<<"Enter x :";
+cin>>x;
+print(x);
+}
